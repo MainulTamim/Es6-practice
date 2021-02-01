d1b6db18d66d39563b5a3442993e5b8ec28785c5
MainulTamim <tamim8332@gmail.com>
2021-02-01 23:58:30 +0600

    js

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(16);
+const nayok = {name:"salman shah", phone:12434};
+
+//console.log(numbers);
+
+let patientName = "Rahim chacha";
+patientName = "Fatema khala"
+console.log(patientName);
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i);
---
+++ template.js
@@ -0,0 +1,8 @@
+const firstName = 'justin';
+const lastName = 'Timberlake';
+const fullName = firstName + ' ' + lastName;
+const fullName2 = `${firstName} ${20+30+40} is a good boy`
+const multiLine = `i love you
+i need you
+i miss tou`
+console.log(multiLine);
